2c1c6d217edcfac4720fbaefa592d068dec5df9d
Romano999 <71527835+Romano999@users.noreply.github.com>
2022-09-29 22:46:40 +0200

    Edit page layout change.

--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -12,54 +12,60 @@
         </h2>
     </x-slot>
 
-    <div>
-        <div>
-            {{-- Canvas --}}
-            <div class="py-12 flex-1">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                        <div>
-                            <h1>{{ __('Create new objects') }}</h1>
-                        </div>
-                        <div class="flex shadow-md sm:rounded-lg px-4 py-4">  
-                            <x-jet-button id="create-text-button" class="flex-1">
-                                {{ __('Create text') }}
-                            </x-jet-button>
-                            <x-jet-button class="flex-1">
-                                {{ __('Create image') }}
-                            </x-jet-button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="flex">
-            {{-- Canvas Item Editor --}}
-            <div class="py-12 flex-1">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                        <div class="relative overflow-x-auto shadow-md sm:rounded-lg px-4 py-4">  
-                            <div id="canvas-edit">
-                                <div>
-                                    <label for="text-font-size">Font size:</label>
-                                    <input type="range" value="" min="1" max="120" step="1" id="text-font-size">
-                                  </div>
+    <div class="flex justify-center items-center">
+        <div class="w-2/3">
+            <div>
+                {{-- Canvas --}}
+                <div class="py-12 flex-1">
+                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                            <div>
+                                <h1>{{ __('Create new objects') }}</h1>
+                            </div>
+                            <div class="flex shadow-md sm:rounded-lg px-4 py-4">  
+                                <x-jet-button id="create-text-button" class="flex-1">
+                                    {{ __('Create text') }}
+                                </x-jet-button>
+                                <x-jet-button class="flex-1">
+                                    {{ __('Create image') }}
+                                </x-jet-button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-    
-            {{-- Canvas --}}
-            <div class="py-12 flex-1">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                        <div class="relative overflow-x-auto shadow-md sm:rounded-lg px-4 py-4">  
-                            <canvas id="canvas" width="1000" height="1000"></canvas>
+            <div class="flex">
+                {{-- Canvas Item Editor --}}
+                <div class="py-12 flex-1">
+                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                            <div class="relative overflow-x-auto shadow-md sm:rounded-lg px-4 py-4">  
+                                <div id="canvas-edit">
+                                    <div>
+                                        <label for="text-font-size">Font size:</label>
+                                        <input type="range" value="" min="1" max="120" step="1" id="text-font-size">
+                                    </div>
+                                    <div>
+                                        <x-jet-button id="delete-text-button" class="flex-1">
+                                            {{ __('Delete text') }}
+                                        </x-jet-button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>    
+                {{-- Canvas --}}
+                <div class="py-12 flex-1">
+                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                            <div class="relative overflow-x-auto shadow-md sm:rounded-lg px-4 py-4">  
+                                <canvas id="canvas" width="1000" height="1000"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>    
+        </div>
     </div>
 </x-app-layout>
