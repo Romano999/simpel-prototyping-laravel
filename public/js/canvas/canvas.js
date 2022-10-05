63fd3d72b489d1b1bbf78c365e46e67df4851dbc
Romano999 <71527835+Romano999@users.noreply.github.com>
2022-10-05 18:38:38 +0200

    Canvas is now moveable and zoomable.

--- public/js/canvas/canvas.js
+++ public/js/canvas/canvas.js
@@ -11,6 +11,7 @@ let canvas_height;
 var page = page;
 var pageObjects = pageObjects;
 var selectedObject;
+var panning = false;
 
 window.addEventListener('DOMContentLoaded', (event) => {
 
@@ -29,6 +30,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
             selectedObject = event.target;
             document.getElementById('canvas-edit').style.display = 'block';
         } else  {
+            panning = true;
             selectedObject = null;
             document.getElementById('canvas-edit').style.display = 'none';
         }
@@ -63,8 +65,68 @@ window.addEventListener('DOMContentLoaded', (event) => {
         delete_object(canvas.getActiveObject());
     };
 
+    // Canvas actions
     canvas.on('object:modified', function(event){
         post_object(event);
+    });
+
+    // Canvas Panning
+    canvas.on('mouse:up', function (e) {
+        panning = false;
+    });
+    
+    // canvas.on('mouse:down', function (e) {
+    //     panning = true;
+    // });
+
+    canvas.on('mouse:move', function (e) {
+        if (panning && e && e.e) {
+            var units = 10;
+            var delta = new fabric.Point(e.e.movementX, e.e.movementY);
+            canvas.relativePan(delta);
+        }
+    });
+    
+    // Canvas zoom
+    canvas.on('mouse:down', function(opt) {
+        var evt = opt.e;
+        if (evt.altKey === true) {
+            this.isDragging = true;
+            this.selection = false;
+            this.lastPosX = evt.clientX;
+            this.lastPosY = evt.clientY;
+        }
+    });
+
+    canvas.on('mouse:move', function(opt) {
+        if (this.isDragging) {
+            var e = opt.e;
+            var vpt = this.viewportTransform;
+            vpt[4] += e.clientX - this.lastPosX;
+            vpt[5] += e.clientY - this.lastPosY;
+            this.requestRenderAll();
+            this.lastPosX = e.clientX;
+            this.lastPosY = e.clientY;
+        }
+    });
+
+    canvas.on('mouse:up', function(opt) {
+        // on mouse up we want to recalculate new interaction
+        // for all objects, so we call setViewportTransform
+        this.setViewportTransform(this.viewportTransform);
+        this.isDragging = false;
+        this.selection = true;
+    });
+
+    canvas.on('mouse:wheel', function(opt) {
+        var delta = opt.e.deltaY;
+        var zoom = canvas.getZoom();
+        zoom *= 0.999 ** delta;
+        if (zoom > 20) zoom = 20;
+        if (zoom < 0.01) zoom = 0.01;
+        canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+        opt.e.preventDefault();
+        opt.e.stopPropagation();
     });
 });
 
